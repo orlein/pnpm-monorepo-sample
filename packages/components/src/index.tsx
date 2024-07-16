023a9cfe2756e08a1cdea61f7496cd77c4621988
orlein <pcj9024@gmail.com>
2024-07-16 22:37:02 +0900

    WIP: must use vite instead of rollup

---
+++ packages/components/src/index.tsx
@@ -0,0 +1,7 @@
+export { default as MuiButton } from "./components/mui/button";
+export {
+  Button as ShadcnButton,
+  type ButtonProps,
+  buttonVariants,
+} from "./components/shadcn/button";
+export { default as PrimeReactButton } from "./components/primereact/button";
