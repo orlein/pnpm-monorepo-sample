297c6ca4c1f0b6a51e73f79bf8ef228c6efa8411
Orlein <pcj9024@gmail.com>
2024-07-17 16:56:17 +0900

    without vite

--- packages/user-frontend/src/app/page.tsx
+++ packages/user-frontend/src/app/page.tsx
@@ -2,10 +2,13 @@ import { MuiButton, PrimeReactButton, ShadcnButton } from "@sample/components";
 
 export default function Home() {
   return (
-    <div className="bg-slate-600">
-      <MuiButton>MuiButton</MuiButton>
-      <PrimeReactButton label="PrimeReactButton" />
-      <ShadcnButton variant={"secondary"}>ShadcnButton</ShadcnButton>
+    <div className="bg-slate-600 w-fit">
+      <div className="flex flex-col gap-4">
+        <MuiButton>MuiButton</MuiButton>
+        <PrimeReactButton label="PrimeReactButton" />
+        <ShadcnButton variant={"default"}>ShadcnButton</ShadcnButton>
+        <ShadcnButton variant={"secondary"}>ShadcnButton</ShadcnButton>
+      </div>
     </div>
   );
 }
